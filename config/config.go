59ddc7d2875e9eee2131214c4a3e2cb7539165f4
Alex <thealexsb@gmail.com>
2021-02-08 20:00:02 +0300

    config logging update

--- config/config.go
+++ config/config.go
@@ -15,25 +15,25 @@ func InitConfiguration() {
 	path = "config.json"
 
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		log.Fatal(err)
+		log.Fatal("config file does not exist: " + err.Error())
 
 		return
 	}
 
 	if jsonFile, err := os.Open(path); err != nil {
-		log.Fatal(err)
+		log.Fatal("failed to open config file: " + err.Error())
 
 		return
 	} else {
 		defer jsonFile.Close()
 
 		if byteValue, err := ioutil.ReadAll(jsonFile); err != nil {
-			log.Fatal(err)
+			log.Fatal("failed to read config file: " + err.Error())
 
 			return
 		} else {
 			if err := json.Unmarshal(byteValue, &Config); err != nil {
-				log.Fatal(err)
+				log.Fatal("failed to parse config file: " + err.Error())
 
 				return
 			}
