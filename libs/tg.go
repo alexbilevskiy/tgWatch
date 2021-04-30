3b941c14c70ed61c9e84b0f7382ab76eebdbc1cc
Alex <thealexsb@gmail.com>
2021-04-30 12:41:06 +0300

    add tdlib options view; show active sessions on startup

--- libs/tg.go
+++ libs/tg.go
@@ -14,6 +14,7 @@ import (
 
 func initTdlib() {
 	LoadChatFilters()
+	loadOptionsList()
 	localChats = make(map[int64]*client.Chat)
 	authorizer := client.ClientAuthorizer()
 	go client.CliInteractor(authorizer)
