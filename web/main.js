4b06a5d240207e43762462f8da92c0c03a908898
Alex <thealexsb@gmail.com>
2021-09-22 16:25:07 +0300

    sortable chatlist

--- web/main.js
+++ web/main.js
@@ -32,3 +32,47 @@ function changePhone(select) {
     document.cookie = 'acc=' + select.value + '; path=/';
     location.reload();
 }
+
+let Table = {
+    tbody: $('#overview_table tbody'),
+    head_rows: $('#overview_table thead .sort_by'),
+    rows: $('#overview_table tbody tr'),
+    sortable: $('#overview_table .sort_by')
+};
+
+let TableHandler = {
+    lastSorted: null,
+
+    sortBy: function (sortBy) {
+        let rows = [].slice.call(Table.rows);
+
+        if (this.lastSorted === sortBy) {
+            rows.sort(function (a, b) {
+                return parseFloat(a.dataset[sortBy]) - parseFloat(b.dataset[sortBy]);
+            });
+            this.lastSorted = null;
+        } else {
+            rows.sort(function (a, b) {
+                return parseFloat(b.dataset[sortBy]) - parseFloat(a.dataset[sortBy]);
+            });
+            this.lastSorted = sortBy;
+        }
+
+        Table.tbody.html('').append(rows);
+    },
+
+    showActiveSortParam: function (sortBy) {
+        $.each(Table.head_rows, function (key, row) {
+            if (row.dataset.sortBy === sortBy) {
+                row.classList.add('bg-success')
+            } else {
+                row.classList.remove('bg-success');
+            }
+        });
+    }
+};
+Table.sortable.on('click', function (e) {
+    TableHandler.showActiveSortParam(e.target.dataset.sortBy);
+    TableHandler.sortBy(e.target.dataset.sortBy);
+});
+
