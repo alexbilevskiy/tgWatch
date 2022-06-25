3b6de2ad8a7cf696b13283dc47f88c32bf746c5c
Alex <thealexsb@gmail.com>
2022-06-25 21:18:20 +0300

    run script upd

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-CGO_CFLAGS="-I/opt/src/td/tdlib/include" CGO_LDFLAGS="-L/opt/src/td/tdlib/lib" go build cmd/tgWatch.go && ./tgWatch
+#!/usr/bin/env bash
+export CGO_CFLAGS="-I/opt/src/td/tdlib/include" CGO_LDFLAGS="-L/opt/src/td/tdlib/lib"
+if [[ $1 ]]
+then
+  echo "single account: $1";
+   go build cmd/tgWatch.go && ./tgWatch $1;
+else
+  echo "all accounts";
+  go build cmd/tgWatch.go && ./tgWatch;
+fi
