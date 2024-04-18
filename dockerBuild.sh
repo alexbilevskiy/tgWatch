6c443a24d9be026360b30b7b9edcadc5d0a7b7b5
Alex <thealexsb@gmail.com>
2024-04-18 21:56:42 +0300

    docker build improve

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+if [[ $DOCKER_REGISTRY == "" ]];
+  then
+    echo "specifiy DOCKER_REGISTRY env"
+    exit
+  else
+    echo "using registry ${DOCKER_REGISTRY}"
+fi
+TAG=`git rev-parse HEAD`
+DATE=`date +%s`
 set -x
-docker build --ulimit memlock=-1 --ulimit nofile=65535:65535 -t bee:5001/tgwatch:latest . && docker push bee:5001/tgwatch:latest
+docker build --ulimit memlock=-1 --ulimit nofile=65535:65535 -t ${DOCKER_REGISTRY}/tgwatch:latest -t ${DOCKER_REGISTRY}/tgwatch:${DATE}_${TAG} . && docker push -a ${DOCKER_REGISTRY}/tgwatch
